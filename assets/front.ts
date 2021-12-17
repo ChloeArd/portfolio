9d68025814e8ac8067a52a99a3f3ba43693b3857
ChloeArd <chloe.ardoise@gmail.com>
2021-12-17 10:00:55 +0100

    portfolio

--- assets/front.ts
+++ assets/front.ts
@@ -5,39 +5,8 @@ import "./images/marvel2.png";
 import "./images/site.png";
 import "./images/photo-1499951360447-b19be8fe80f5.png";
 import "./images/photo-1488590528505-98d2b5aba04b.png";
-
-class Animation {
-
-    public observer(): void {
-        // creation of an intersection observer
-        const ratio: number = .1;
-        const options : {
-            root: null;
-            rootMargin: string;
-            threshold: number
-        } = {
-            root: null,
-            rootMargin: '0px',
-            threshold: .1
-        }
-
-        // Allows to make visible the elements according to the scroll.
-        const handleIntersect = function (entries: any[], observer: { unobserve: (arg0: any) => void; }) {
-            entries.forEach(function (entry) {
-                if (entry.intersectionRatio > ratio) {
-                    entry.target.classList.add('reveal-visible');
-                    observer.unobserve(entry.target);
-                }
-            });
-        }
-
-        const observer = new IntersectionObserver(handleIntersect, options);
-        // The element to observe to make the animation
-        document.querySelectorAll('.reveal').forEach(function (r) {
-            observer.observe(r);
-        });
-    }
-}
+// @ts-ignore
+import {Animation} from "./ts/Animation.ts";
 
 let animation : Animation = new Animation();
 animation.observer();
--- public/View/_Partials/structureView.php
+++ public/View/_Partials/structureView.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="../../build/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/351e9300a0.js" crossorigin="anonymous"></script>
-    <script src="../../build/js/front.js" defer></script>
+    <script type="module" src="../../build/js/front.js" defer></script>
 </head>
 <body>
 
